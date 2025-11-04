ec45a6e02947b4491aceeebc368e39707f6691d8
edithmb <gmirandab2425@politecnics.barcelona>
2025-11-04 17:50:01 +0100

    Creación de los layout de todas las activities.

---
+++ app/src/main/java/com/example/hophop/Cuenta_Regresiva.kt
@@ -0,0 +1,14 @@
+package com.example.hophop
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Cuenta_Regresiva : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cuenta_regresiva)
+    }
+}
