c60a734e25e271a39c961fa062f5290af8d73081
edithmb <gmirandab2425@politecnics.barcelona>
2025-11-07 18:00:11 +0100

    Creacion de las activities inicio y login

--- app/src/main/java/com/example/hophop/Inicio.kt
+++ app/src/main/java/com/example/hophop/Inicio.kt
@@ -1,6 +1,8 @@
 package com.example.hophop
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +12,14 @@ class Inicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
+
+        val btnEmpezar = findViewById<Button>(R.id.btnEmpezar)
+
+        btnEmpezar.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+
+        }
 
     }
 }
--- app/src/main/java/com/example/hophop/Login.kt
+++ app/src/main/java/com/example/hophop/Login.kt
@@ -1,6 +1,8 @@
 package com.example.hophop
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +12,14 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnContinuar = findViewById<Button>(R.id.btnContinuar)
+
+        btnContinuar.setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+
+        }
 
     }
 }
