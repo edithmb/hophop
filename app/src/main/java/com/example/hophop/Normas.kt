0f654d5e30367573586f18d40c611e181367379b
Ledys2312 <lmaradiagas2425@politecnics.barcelona>
2025-11-10 18:48:00 +0100

    Creación de ventana elección y ventana normas.

--- app/src/main/java/com/example/hophop/Normas.kt
+++ app/src/main/java/com/example/hophop/Normas.kt
@@ -1,14 +1,25 @@
 package com.example.hophop
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Normas : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_normas)
+
+        val btnContinuarNormas = findViewById<Button>(R.id.btnContinuarN)
+
+        btnContinuarNormas.setOnClickListener {
+            val intent = Intent(this, Cuenta_Regresiva::class.java)
+            startActivity(intent)
+        }
     }
 }
