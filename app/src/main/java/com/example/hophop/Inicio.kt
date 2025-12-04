7ff1fdfb11694c84e54ab13e5930aa42f136b293
edithmb <gmirandab2425@politecnics.barcelona>
2025-12-04 19:06:21 +0100

    activity metricas y boton ajustes

--- app/src/main/java/com/example/hophop/Inicio.kt
+++ app/src/main/java/com/example/hophop/Inicio.kt
@@ -2,21 +2,29 @@ package com.example.hophop
 
 import android.app.Dialog
 import android.content.Intent
+import android.media.Image
 import android.os.Bundle
 import android.view.Window
 import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class Inicio : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
 
         val btnEmpezar = findViewById<Button>(R.id.btnEmpezar)
         val btnSalirI = findViewById<Button>(R.id.btnSalirI)
+        val ajustes = findViewById<ImageView>(R.id.ImageViewAjustes)
 
         btnEmpezar.setOnClickListener {
             val intent = Intent(this, Login::class.java)
@@ -28,6 +36,11 @@ class Inicio : AppCompatActivity() {
             mostrarMensajeSalir()
         }
 
+        ajustes.setOnClickListener {
+            if (!drawerLayout.isDrawerOpen(GravityCompat.END)){
+                drawerLayout.openDrawer(GravityCompat.END)
+            }
+        }
 
     }
 
